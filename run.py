9ebeb2be8c08d46ab3ae0f135d659db19cc83ade
nozidoali <nozidoali@gmail.com>
2024-09-06 23:25:39 +0200

    update: strash benchmark

---
+++ run.py
@@ -0,0 +1,26 @@
+from backend import read_blif, LPManager
+
+def retime(inputFile, outputFile, clockPeriod):
+    
+    graph = read_blif(inputFile)
+    manager = LPManager()
+    manager.setClockPeriod(float(clockPeriod))
+    manager.linkSubjectGraph(graph)
+    manager.addObjective()
+    manager.solve()
+    manager.insertBuffers()
+    manager.dumpGraph(outputFile)
+    
+    latency = manager.getLatency()
+    print(int(latency))
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Retime the BLIF file")
+    parser.add_argument("-i", "--input", help="Input BLIF file")
+    parser.add_argument("-o", "--output", help="Output BLIF file")
+    parser.add_argument("-c", "--clock", help="Clock period")
+    args = parser.parse_args()
+    retime(args.input, args.output, args.clock)
+    
