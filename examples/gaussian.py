9ba19b674d36cda3a209fe4a4832e81f13f925bc
hanyu <nozidoali@gmail.com>
2024-11-07 14:04:11 -0800

    update: add exmaples to show how to printing variables

--- examples/gaussian.py
+++ examples/gaussian.py
@@ -6,6 +6,7 @@ def main():
     input_file = "examples/gaussian/gaussian.blif"
     input_sched_constr = "examples/gaussian/gaussian.json"
     output_file = "examples/gaussian/gaussian_opt.blif"
+    solution_file = "examples/gaussian/gaussian_sol.sol"
 
     graph = read_blif(input_file)
     model = MapBufModel(graph, json.load(open(input_sched_constr)), 4.2, {"maxLeaves": 6})
@@ -14,7 +15,11 @@ def main():
     # print some stats
     print(f"Latency: {model.getLatency()}")
     model.dumpGraph(output_file)
+    model.dumpModel(solution_file)
 
+    # print the l labels
+    for signal, idx in model.signal2idx.items():
+        print(f"{signal}: {model.model.getVarByName(f'l_{idx}').X}")
 
 if __name__ == "__main__":
     main()
